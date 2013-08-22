02e3a86fe65ae5931b88640eb6605439733179b3
Marlon <marlonmisra@gmail.com>
2013-08-21 23:07:00 -0400

    Using yield and provide

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Campusdevs::Application.routes.draw do
+  root to: 'static_pages#home'
+
+  get '/about',    to: 'static_pages#about'
+  get '/team',     to: 'static_pages#team'
+  get '/contact',  to: 'static_pages#contact'
+end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +60,3 @@ Campusdevs::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
